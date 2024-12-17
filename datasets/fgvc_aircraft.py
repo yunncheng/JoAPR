4a6d926a713daf070646ea52ab139f990de5600a
Yuncheng Guo <yunncheng@163.com>
2024-12-17 20:15:47 +0800

    Added FGVCAircraft dataset files and updated README with configs for various datasets

---
+++ datasets/fgvc_aircraft.py
@@ -0,0 +1,99 @@
+import os
+import pickle
+
+from dassl.data.datasets import DATASET_REGISTRY, Datum, DatasetBase
+from dassl.utils import mkdir_if_missing
+
+from .add_noise import generate_fewshot_dataset_with_symflip_noise, generate_fewshot_dataset_with_pairflip_noise
+
+from .oxford_pets import OxfordPets
+
+
+@DATASET_REGISTRY.register()
+class FGVCAircraft(DatasetBase):
+
+    dataset_dir = "fgvc_aircraft"
+
+    def __init__(self, cfg, pred=[], prob=[]):
+        root = os.path.abspath(os.path.expanduser(cfg.DATASET.ROOT))
+        self.dataset_dir = os.path.join(root, self.dataset_dir)
+        self.image_dir = os.path.join(self.dataset_dir, "images")
+        self.split_fewshot_dir = os.path.join(self.dataset_dir, "split_fewshot")
+        mkdir_if_missing(self.split_fewshot_dir)
+
+        classnames = []
+        with open(os.path.join(self.dataset_dir, "variants.txt"), "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                classnames.append(line.strip())
+        cname2lab = {c: i for i, c in enumerate(classnames)}
+
+        train = self.read_data(cname2lab, "images_variant_train.txt")
+        val = self.read_data(cname2lab, "images_variant_val.txt")
+        test = self.read_data(cname2lab, "images_variant_test.txt")
+
+        num_shots = cfg.DATASET.NUM_SHOTS
+        if num_shots >= 1:
+            seed = cfg.SEED
+            preprocessed = os.path.join(self.split_fewshot_dir, f"shot_{num_shots}-seed_{seed}.pkl")
+            
+            if os.path.exists(preprocessed):
+                print(f"Loading preprocessed few-shot data from {preprocessed}")
+                with open(preprocessed, "rb") as file:
+                    data = pickle.load(file)
+                    train, val = data["train"], data["val"]
+            else:
+                if cfg.DATASET.FP_TYPE == "symflip":
+                    train = generate_fewshot_dataset_with_symflip_noise(train, num_shots=num_shots,
+                                                                        num_fp=cfg.DATASET.NUM_FP, seed=seed)
+                elif cfg.DATASET.FP_TYPE == "pairflip":
+                    train = generate_fewshot_dataset_with_pairflip_noise(train, num_shots=num_shots,
+                                                                         num_fp=cfg.DATASET.NUM_FP, seed=seed)
+                else:
+                    raise ValueError(f"There is no such type of noise!")
+                val = self.generate_fewshot_dataset(val, num_shots=min(num_shots, 4))
+                data = {"train": train, "val": val}
+                print(f"Saving preprocessed few-shot data to {preprocessed}")
+                with open(preprocessed, "wb") as file:
+                    pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+        subsample = cfg.DATASET.SUBSAMPLE_CLASSES
+        train, val, test = OxfordPets.subsample_classes(train, val, test, subsample=subsample)
+
+        if len(pred) > 0:
+            pred_idx = pred.nonzero()[0]
+            train_x = [train[i] for i in pred_idx]
+            self.probability_x = [prob[i] for i in pred_idx]
+            print("clean data has a size of %d" % (len(train_x)))
+
+            pred_idx = (1 - pred).nonzero()[0]
+            train_u = [train[i] for i in pred_idx]
+            self.probability_u = [prob[i] for i in pred_idx]
+            print("noisy data has a size of %d" % (len(train_u)))
+
+        else:
+            train_x = train
+            train_u = None
+
+        super().__init__(train_x=train_x, train_u=train_u, val=val, test=test)
+        label_set = set()
+        for item in train:
+            label_set.add(item.label)
+        self._num_classes = max(label_set) + 1
+
+    def read_data(self, cname2lab, split_file):
+        filepath = os.path.join(self.dataset_dir, split_file)
+        items = []
+
+        with open(filepath, "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip().split(" ")
+                imname = line[0] + ".jpg"
+                classname = " ".join(line[1:])
+                impath = os.path.join(self.image_dir, imname)
+                label = cname2lab[classname]
+                item = Datum(impath=impath, label=label, classname=classname)
+                items.append(item)
+
+        return items
